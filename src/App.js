54dd8c4090530bdeda73465530639889c19b8b09
manhquang00m <vumanhquang36@gmail.com>
2022-05-10 14:05:54 +0700

    edit appjs

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import Portfolio from './portfolio/Portfolio';
 import Project from './portfolio/Project';
 import ToDoList from './react-cyberlearn_3/JSS_StyledComponent/ToDoList/ToDoList';
-import DemoUseSpring from './hook/react-spring/DemoUseSpring';
-import DemoReduxApp from './hook/DemoReduxApp';
 import BaiTapGameBauCua from './BaiTapTongHop/GameBauCua/BaiTapGameBauCua';
 import { Route, Routes } from 'react-router-dom';
 import RenderMovie from './react_cyberlearn_1/renderMovie/RenderMovie';
@@ -12,15 +10,10 @@ import DatVeXemFilm from './react-cyberlearn_2/datVeXemFilm/DatVeXemFilm';
 function App() {
   return (
     <div className="App">
-      {/* <Portfolio /> */}
-      {/* <ToDoList /> */}
-      {/* <DemoUseSpring /> */}
-      {/* <DemoReduxApp /> */}
-      {/* <BaiTapGameBauCua /> */}
       <Routes>
-        <Route path='/' element={<Portfolio />} />
-        <Route path='/project' element={<Project />} />
-        <Route path='/BaiTapGameBauCua' element={<BaiTapGameBauCua />} />
+        <Route path="/" element={<Portfolio />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/BaiTapGameBauCua" element={<BaiTapGameBauCua />} />
         <Route path="/rendermovie" element={<RenderMovie />} />
         <Route path="/datvexemfilm" element={<DatVeXemFilm />} />
         <Route path="/todolist" element={<ToDoList />} />
--- src/index.js
+++ src/index.js
@@ -12,16 +12,15 @@ import { createStore } from 'redux'
 import rootReducer from './redux/rootReducer'
 const store = createStore(rootReducer)
 
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Router>
       <Provider store={store}>
         <App />
       </Provider>
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
